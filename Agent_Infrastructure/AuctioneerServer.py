5aeef42ede71c642ab82cb26dae6ec494cc3af2d
Shachar Dan <shachar.dan@tuhh.de>
2024-05-07 11:33:34 +0200

    update main file for auctioneer

---
+++ Agent_Infrastructure/AuctioneerServer.py
@@ -0,0 +1,17 @@
+from auctioneer import *
+import time
+
+if __name__ == "__main__":
+    server = Auctioneer(host=socket.gethostname(), port=40000)
+
+    server_thread = threading.Thread(target=server.start)
+    server_thread.start()
+    
+    # wait for x minutes
+    time.sleep(60)
+    
+    # send transport request to first registered client
+    if server.connections:
+        server.get_TR_from_carrier(server.connections[0][1])
+
+    else: print("No carriers connected to network")
